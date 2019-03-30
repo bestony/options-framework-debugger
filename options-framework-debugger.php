c1db81ee14437d923ecb7226b5e737fe189f43ee
Bestony <best.tony@foxmail.com>
2019-03-30 22:12:43 +0800

    init

---
+++ options-framework-debugger.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Options Framework Debugger
+Plugin URI: https://wpstore.app/archives/options-framework-debugger/
+Description: 这是一个 copyright 插件，能够在文章尾部添加内容。
+Version: 1.0.0
+Author: Bestony
+Author URI: https://wpstore.app/
+*/
